0fc38cd73ba4680195462859ad37493b74db52e1
IOU4 <ouchaibimad000@gmail.com>
2022-11-03 12:06:27 +0100

    use of .env

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,9 +6,16 @@ java { sourceCompatibility = JavaVersion.VERSION_19 }
 
 dependencies { 
   implementation("org.postgresql:postgresql:42.5.0")
-  implementation("org.eclipse.angus:jakarta.mail:1.0.0")
+    implementation("org.eclipse.angus:jakarta.mail:1.0.0")
 }
 
-tasks { named<JavaExec>("run") { standardInput = System.`in` } }
+tasks { 
+  named<JavaExec>("run") {
+    standardInput = System.`in` 
+    file(".env").readLines().forEach {
+        environment(it.split("=")[0], it.split("=")[1])
+      }
+  } 
+}
 
 application { mainClass.set("simplonclone.Main") }
